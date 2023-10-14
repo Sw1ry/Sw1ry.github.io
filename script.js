56f13a1af428faf7e7d098af53d4ad22b59a2aec
gabsdev <107257018+thegabsdev@users.noreply.github.com>
2023-10-14 08:54:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('ilMioLink').addEventListener('click', function() {
     // Sostituisci 'URL_DEL_TUO_LINK' con il link a cui vuoi che il pulsante ti porti.
-    window.location.href = 'https://pornhub.it/';
+    window.location.href = 'https://youtu.be/dQw4w9WgXcQ?feature=shared';
 });
