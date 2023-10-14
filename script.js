25e8338ccbb393ac5e27a0b20c501a9cfe1aee4d
Sw1ry <107684107+Sw1ry@users.noreply.github.com>
2023-10-14 09:39:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('ilMioLink').addEventListener('click', function() {
-    window.location.href = 'https://youtu.be/dQw4w9WgXcQ?si=7zMFM3TKh-nU9dKS'; // Sostituisci con il tuo link desiderato.
+    window.location.href = 'https://youtu.be/xvFZjo5PgG0?si=MCIxKXcL1MeFWYz7'; // Sostituisci con il tuo link desiderato.
 });
