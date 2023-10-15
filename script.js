8a3833e6efebb6f6a0e56b5f629c11a9d43866b8
Kalium <87941224+Kalium919@users.noreply.github.com>
2023-10-15 11:53:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const loginButton = document.getElementById("login-button");
-
-loginButton.addEventListener("mouseover", () => {
-  loginButton.style.backgroundColor = "#27ae60";
-});
-
-loginButton.addEventListener("mouseout", () => {
-  loginButton.style.backgroundColor = "#2ecc71";
-});
-
-loginButton.addEventListener("click", () => {
-  loginButton.disabled = false;
-  loginButton.style.opacity = "0.7";
-  loginButton
-});
-
 window.onload = function() {
   var audio = new Audio("/extra/Undertale OST 023 - Shop.mp3");
   audio.play();
